1bf36f276e7e0f490542558823c787dd0534e078
Renan Mascarenhas <rgoncalves2@uolinc.com>
2025-05-26 16:40:01 -0300

    feat: Home page com cartões de populares estilizada, mas ainda com mocks.

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,46 @@
-import React from 'react'
-import {Button} from "@/components/ui/button";
+import CompanionCard from "@/components/CompanionCard";
+import CompanionsList from "@/components/CompanionsList";
+import CTA from "@/components/CTA";
+import React from "react";
 
 const Page = () => {
   return (
-    <div>
-      <h1 className="text-2xl underline">Welcome to my SaaS App</h1>
-      <Button>Let's get started</Button>
-    </div>
-  )
-}
+    <main>
+      <h1 className="text-2xl underline">Popular Companions</h1>
+      <section className="home-section">
+        <CompanionCard
+          id="123"
+          name="Neura the Brainy Explorer"
+          topic="Neural Network of the Brain"
+          subject="science"
+          duration={45}
+          color="#E5D0FF"
+        />
 
-export default Page
+        <CompanionCard
+          id="345"
+          name="Countsy the Number Wizard"
+          topic="Derivatives & Integrals"
+          subject="maths"
+          duration={30}
+          color="#FFDA6E"
+        />
+
+        <CompanionCard
+          id="789"
+          name="Verba the Vocabulary Builder"
+          topic="English Literature "
+          subject="Language"
+          duration={30}
+          color="#BDE7FF"
+        />
+      </section>
+      <section className="home-section">
+        <CompanionsList />
+        <CTA />
+      </section>
+    </main>
+  );
+};
+
+export default Page;
